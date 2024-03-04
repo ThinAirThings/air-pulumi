5be4b92e75c1ae6278d58d49d10a76017d148815
irishcarbomb777 <danielclannan@gmail.com>
2024-03-04 17:30:37 -0500

    mod

---
+++ src/components/PyLambdaCallbackFactory.ts
@@ -0,0 +1,75 @@
+import * as aws from "@pulumi/aws";
+import { createNameTag } from "../utils/createNameTag";
+import { ZodType, z, infer as InferType } from "zod";
+import { Input } from "@pulumi/pulumi";
+import * as pulumi from "@pulumi/pulumi";
+
+export const PyLambdaCallbackFactory =
+    (applicationIamUser: aws.iam.User) =>
+    ({
+        tag,
+        codePath,
+        environmentVariables,
+    }: {
+        tag: string;
+        codePath: string;
+        environmentVariables?: Input<Record<string, Input<string>>>;
+    }) => {
+        // Create nametag
+        const nameTag = createNameTag(tag).replaceAll("_", "-");
+        // Create Lambda Permissions
+        // Configure IAM so that the AWS Lambda can be run.
+        const lambdaRole = new aws.iam.Role(`${nameTag}_lambda_role`, {
+            assumeRolePolicy: {
+                Version: "2012-10-17",
+                Statement: [{
+                    Action: "sts:AssumeRole",
+                    Principal: {
+                        Service: "lambda.amazonaws.com",
+                    },
+                    Effect: "Allow",
+                    Sid: "",
+                }],
+            },
+        })
+        // Create Lambda Policy
+        new aws.iam.RolePolicyAttachment(`${nameTag}_lambda_policy_attachment`, {
+            role: lambdaRole,
+            policyArn: aws.iam.ManagedPolicy.LambdaFullAccess,
+        })
+        // Create Lambda
+
+        const lambda = new aws.lambda.Function(`${nameTag}-lambda`, {
+            role: lambdaRole.arn,
+            runtime: aws.lambda.Runtime.Python3d11,
+            timeout: 60 * 15,
+            memorySize: 10240,
+            environment: {
+                variables: {
+                    ...environmentVariables,
+                },
+            },
+            code: new pulumi.asset.AssetArchive({
+                ".": new pulumi.asset.FileArchive(codePath),
+            }),
+        });
+        // Set Permissions
+        new aws.iam.UserPolicyAttachment(`${nameTag}_policy_attachment`, {
+            user: applicationIamUser.name,
+            policyArn: new aws.iam.Policy(`${nameTag}_policy`, {
+                policy: lambda.arn.apply((arn) =>
+                    JSON.stringify({
+                        Version: "2012-10-17",
+                        Statement: [
+                            {
+                                Effect: "Allow",
+                                Action: ["lambda:InvokeFunction"],
+                                Resource: arn,
+                            },
+                        ],
+                    }),
+                ),
+            }).arn,
+        });
+        return lambda;
+    };
