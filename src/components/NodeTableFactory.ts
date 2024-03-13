1fccfc0d59de68f1b3e51ee330abf9fc4dd88896
irishcarbomb777 <danielclannan@gmail.com>
2024-03-13 14:02:32 -0400

    mod

--- src/components/NodeTableFactory.ts
+++ src/components/NodeTableFactory.ts
@@ -5,13 +5,13 @@ import { DynamoTableFactory } from "./DynamoTableFactory";
 
 export const NodeTableFactory = (
     DynamoTable: ReturnType<typeof DynamoTableFactory>
-) => <T extends AirNode<any, any, any>>({
-    nodeType,
+) => <T extends AirNode<any, any>>(
+    nodeType: T extends AirNode<any, infer U, any> ? U : never
+    ,{
     version,
     streamEnabled,
     streamViewType
 }: {
-    nodeType: T extends AirNode<any, infer U, any> ? U : never
     version?: number
     streamEnabled?: boolean
     streamViewType?: "NEW_IMAGE" | "OLD_IMAGE" | "NEW_AND_OLD_IMAGES" | "KEYS_ONLY"
