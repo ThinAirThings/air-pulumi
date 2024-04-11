91b3438661cef14a96e2059bd39497dbc8a73ffd
irishcarbomb777 <danielclannan@gmail.com>
2024-04-11 14:56:04 -0400

    fix: add S3Bucket component to index export

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@
 export { GitHubRepository } from "./components/GithubRepository";
 export { TSLambdaCallbackFactory } from "./components@next/TSLambdaCallbackFactory";
 export { IamUser } from "./components@next/IamUser";
+export { S3Bucket } from "./components@next/S3Bucket";
